a7810f9e6df9e83b59a237ccfafc1c91b5924487
riisatoro <riisa.toro@gmail.com>
2022-03-29 16:59:44 +0300

    Add template view for Products

--- web/products/views.py
+++ web/products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from time import sleep
 
-# Create your views here.
+from django.views.generic import ListView
+
+from products.models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+    paginate_by = 50
+    allow_empty = True
+
+    def get(self, *args, **kwargs):
+        sleep(2)
+        return super().get(args, kwargs)
