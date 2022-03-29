09cfcbf0b1d558e75e53549b2dd30fae7c8847f5
riisatoro <riisa.toro@gmail.com>
2022-03-29 17:17:15 +0300

    Add caching page

--- web/products/urls.py
+++ web/products/urls.py
@@ -1,7 +1,10 @@
+from django.conf.global_settings import CACHE_TIME_SEC
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from products.views import ProductListView
 
+
 urlpatterns = [
-    path('', ProductListView.as_view())
+    path('', cache_page(CACHE_TIME_SEC)(ProductListView.as_view()), name='products')
 ]
