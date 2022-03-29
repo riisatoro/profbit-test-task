ee8808d7cc4989c1388f7dba6573bb9134cc023e
riisatoro <riisa.toro@gmail.com>
2022-03-29 23:08:26 +0300

    Add product list with pagination

--- web/products/urls.py
+++ web/products/urls.py
@@ -1,10 +1,8 @@
-from django.conf import settings
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from products.views import ProductListView
 
 
 urlpatterns = [
-    path('', cache_page(settings.CACHE_TIME_SEC)(ProductListView.as_view()), name='products')
+    path('', ProductListView.as_view(), name='products')
 ]
--- web/products/views.py
+++ web/products/views.py
@@ -1,5 +1,6 @@
 from time import sleep
 
+from django.conf import settings
 from django.views.generic import ListView
 
 from products.models import Product
@@ -10,6 +11,11 @@ class ProductListView(ListView):
     paginate_by = 50
     allow_empty = True
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['CACHE_TIME'] = settings.CACHE_TIME_SEC
+        return context
+
     def get(self, *args, **kwargs):
         sleep(2)
-        return super().get(args, kwargs)
+        return super().get(*args, **kwargs)
